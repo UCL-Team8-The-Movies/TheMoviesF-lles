ee03fad771d729eae93345044e336f9095d25df2
Jeppe Lynge Hansen <jklh62358@edu.ucl.dk>
2024-08-15 10:48:06 +0200

    Properties Tiløjet

--- TheMovies/Models/Movie.cs
+++ TheMovies/Models/Movie.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace TheMovies.Models;
 
-namespace TheMovies.Models
+public class Movie
 {
-    internal class Movie
-    {
-    }
+
+    public string Title { get; set; }
+    public int Duration { get; set; }
+    public string Genre { get; set; }
+
+
+
 }
