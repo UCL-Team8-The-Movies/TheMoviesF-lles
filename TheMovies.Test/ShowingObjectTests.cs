7e201377c8573284364f031511f513450e54c612
Jeppe Lynge Hansen <jklh62358@edu.ucl.dk>
2024-08-28 14:02:38 +0200

    feat: SaveAndLoadFromCsvFile test method now works.

--- TheMovies.Test/ShowingObjectTests.cs
+++ TheMovies.Test/ShowingObjectTests.cs
@@ -42,10 +42,10 @@ public class ShowingObjectTests
                 Name = "CineMaxx",
                 City = "Aarhus",
                 MaxCinemaHall = 2,
-                CinemaHalls = new List<string> { "Sal1", "Sal2" }
+                CinemaHalls = new List<string> { "Sal 1", "Sal 2" }
             },
 
-            CinemaHall = "Sal1",
+            CinemaHall = "Sal 1",
             ShowingDuration = 190,
             ShowingDate = new DateTime(2024, 8, 28, 2, 30, 00)
 
@@ -62,12 +62,12 @@ public class ShowingObjectTests
                 MaxCinemaHall = 3,
                 CinemaHalls = new List<string>
                 {
-                    "Sal1",
-                    "Sal2",
-                    "Sal3"
+                    "Sal 1",
+                    "Sal 2",
+                    "Sal 3"
                 }
             },
-            SelectedCinemaHall = "Sal3",
+            SelectedCinemaHall = "Sal 3",
             SelectedMovie = new MovieViewModel(movie),
             SelectedDate = new DateTime(2024, 8, 28, 2, 30, 00),
             TimeOfDay = "10:00"
@@ -111,14 +111,14 @@ public class ShowingObjectTests
         Assert.AreEqual(2, showingList[0].Cinema.MaxCinemaHall);
         Assert.AreNotEqual(10, showingList[0].Cinema.MaxCinemaHall);
 
-        Assert.AreEqual("Sal1", showingList[0].Cinema.CinemaHalls[0]);
-        Assert.AreNotEqual("Sal2", showingList[0].Cinema.CinemaHalls[0]);
+        Assert.AreEqual("Sal 1", showingList[0].Cinema.CinemaHalls[0]);
+        Assert.AreNotEqual("Sal 2", showingList[0].Cinema.CinemaHalls[0]);
 
 
 
         //Assert CinemaHall
-        Assert.AreEqual("Sal1", showingList[0].CinemaHall);
-        Assert.AreNotEqual("Sal2", showingList[0].CinemaHall);
+        Assert.AreEqual("Sal 1", showingList[0].CinemaHall);
+        Assert.AreNotEqual("Sal 2", showingList[0].CinemaHall);
 
         //Assert ShowingDuration
         Assert.AreEqual(190, showingList[0].ShowingDuration);
@@ -131,118 +131,120 @@ public class ShowingObjectTests
 
     }
 
-    //[TestMethod]
-    //public void AddShowingMethodInShowingPageViewModel()
-    //{
-       
+    [TestMethod]
+    public void AddShowingMethodInShowingPageViewModel()
+    {
 
-    //    SPVM.showingRepo.RemoveAll();
+        SPVM.showingRepo.RemoveAll();
 
-    //    //Tilf�j showing til ShowingPageViewModel's showingrepo
-    //    SPVM.AddShowing();
+        //Tilf�j showing til ShowingPageViewModel's showingrepo
+        SPVM.AddShowing();
 
-    //    //retunerer IEnumerable med alle film i ShowingPageViewModel's showingrepo
-    //    var showings = SPVM.showingRepo.GetAll();
+        //retunerer IEnumerable med alle film i ShowingPageViewModel's showingrepo
+        var showings = SPVM.showingRepo.GetAll();
 
-    //    //Konvert�r IEnumerable til List
-    //    List<Showing> showingList = showings.ToList();
+        //Konvert�r IEnumerable til List
+        List<Showing> showingList = showings.ToList();
 
-    //    Assert.AreEqual("hej", showingList[0].Title);
-    //    Assert.AreNotEqual("hello", showingList[0].Movie.Title);
+        //Assert Movie Object
+        Assert.AreEqual("hej", showingList[0].Movie.Title);
+        Assert.AreNotEqual("hello", showingList[0].Movie.Title);
 
-    //    Assert.AreEqual(120, showingList[0].Duration);
-    //    Assert.AreNotEqual(90, showingList[0].Duration);
+        Assert.AreEqual(120, showingList[0].Movie.Duration);
+        Assert.AreNotEqual(90, showingList[0].Movie.Duration);
 
-    //    Assert.AreEqual("action", showingList[0].Genre);
-    //    Assert.AreNotEqual("ation", showingList[0].Genre);
+        Assert.AreEqual("action", showingList[0].Movie.Genre);
+        Assert.AreNotEqual("ation", showingList[0].Movie.Genre);
 
-    //    Assert.AreEqual("John Smith", showingList[0].Director);
-    //    Assert.AreNotEqual("Jesper Hansen", showingList[0].Director);
+        Assert.AreEqual("John Smith", showingList[0].Movie.Director);
+        Assert.AreNotEqual("Jesper Hansen", showingList[0].Movie.Director);
 
-    //    Assert.AreEqual(new DateTime(2024, 8, 28, 2, 30, 00), showingList[0].PremierDate);
-    //    Assert.AreNotEqual(new DateTime(2004, 8, 28, 4, 45, 36), showingList[0].PremierDate);
-
-    //}
-
-    //[TestMethod]
-    //public void AddMovieWithoutTitle_CheckForException()
-    //{
-
-    //    MoviePageViewModel moviePageViewModel = new MoviePageViewModel();
-
-    //    moviePageViewModel.Title = null;
-    //    moviePageViewModel.Duration = 120;
-    //    moviePageViewModel.Genre = "action";
-    //    moviePageViewModel.Director = "John Smith";
-    //    moviePageViewModel.PremierDate = new DateTime(2024, 8, 28, 2, 30, 00);
+        Assert.AreEqual(new DateTime(2024, 8, 28, 2, 30, 00), showingList[0].Movie.PremierDate);
+        Assert.AreNotEqual(new DateTime(2004, 8, 28, 4, 45, 36), showingList[0].Movie.PremierDate);
 
 
-    //    Assert.ThrowsException<ArgumentException>(() => moviePageViewModel.AddMovie());
 
-    //}
+        // Assert Cinema Object
+        Assert.AreEqual("CineMaxx", showingList[0].Cinema.Name);
+        Assert.AreNotEqual("Nordic Film", showingList[0].Cinema.Name);
 
-    //[TestMethod]
-    //public void SaveAndLoadFromCsvFile()
-    //{
+        Assert.AreEqual("Odense", showingList[0].Cinema.City);
+        Assert.AreNotEqual("Aarhus", showingList[0].Cinema.City);
 
-    //    Movie movie1 = new Movie()
-    //    {
-    //        Title = "hej",
-    //        Duration = 120,
-    //        Genre = "action",
-    //        Director = "John Smith",
-    //        PremierDate = new DateTime(2024, 8, 28, 2, 30, 00)
-    //    };
-    //    Movie movie2 = new Movie()
-    //    {
-    //        Title = "Inception",
-    //        Duration = 140,
-    //        Genre = "Sci-Fi",
-    //        Director = "Hans Hansen",
-    //        PremierDate = new DateTime(2022, 9, 23, 5, 20, 00)
+        Assert.AreEqual(3, showingList[0].Cinema.MaxCinemaHall);
+        Assert.AreNotEqual(10, showingList[0].Cinema.MaxCinemaHall);
 
-    //    };
-    //    Movie movie3 = new Movie()
-    //    {
-    //        Title = "Tom and Jerry",
-    //        Duration = 80,
-    //        Genre = "Drama",
-    //        Director = "Eric Ericson",
-    //        PremierDate = new DateTime(2028, 6, 1, 3, 00, 00)
-    //    };
+        Assert.AreEqual("Sal 1", showingList[0].Cinema.CinemaHalls[0]);
+        Assert.AreNotEqual("Sal 2", showingList[0].Cinema.CinemaHalls[0]);
 
-    //    MovieRepo movieRepo1 = new MovieRepo();
-    //    MovieRepo movieRepo2 = new MovieRepo();
 
-    //    movieRepo1.Add(movie1);
-    //    movieRepo1.Add(movie2);
-    //    movieRepo1.Add(movie3);
 
-    //    movieRepo1.SaveToFile();
+        //Assert CinemaHall
+        Assert.AreEqual("Sal 3", showingList[0].CinemaHall);
+        Assert.AreNotEqual("Sal 2", showingList[0].CinemaHall);
 
-    //    movieRepo2.LoadFromFile();
+        //Assert ShowingDuration
+        Assert.AreEqual(150, showingList[0].ShowingDuration);
+        Assert.AreNotEqual(220, showingList[0].ShowingDuration);
 
-    //    List<Movie> moviesList1 = movieRepo1.GetAll().ToList();
-    //    List<Movie> moviesList2 = movieRepo2.GetAll().ToList();
+        //Assert ShowingDate
+        Assert.AreEqual(new DateTime(2024, 8, 28, 2, 30, 00), showingList[0].ShowingDate);
+        Assert.AreNotEqual(new DateTime(2026, 10, 12, 5, 30, 00), showingList[0].ShowingDate);
 
-    //    Assert.AreEqual(moviesList1[0].Title, moviesList2[0].Title);
-    //    Assert.AreEqual(moviesList1[0].Duration, moviesList2[0].Duration);
-    //    Assert.AreEqual(moviesList1[0].Genre, moviesList2[0].Genre);
-    //    Assert.AreEqual(moviesList1[0].Director, moviesList2[0].Director);
-    //    Assert.AreEqual(moviesList1[0].PremierDate, moviesList2[0].PremierDate);
+        //Assert ShowingDuration is 30 longer than the Movie's Duration
+        Assert.AreEqual(showingList[0].ShowingDuration, showingList[0].Movie.Duration + 30);
 
-    //    Assert.AreEqual(moviesList1[1].Title, moviesList2[1].Title);
-    //    Assert.AreEqual(moviesList1[1].Duration, moviesList2[1].Duration);
-    //    Assert.AreEqual(moviesList1[1].Genre, moviesList2[1].Genre);
-    //    Assert.AreEqual(moviesList1[1].Director, moviesList2[1].Director);
-    //    Assert.AreEqual(moviesList1[1].PremierDate, moviesList2[1].PremierDate);
+    }
 
-    //    Assert.AreEqual(moviesList1[2].Title, moviesList2[2].Title);
-    //    Assert.AreEqual(moviesList1[2].Duration, moviesList2[2].Duration);
-    //    Assert.AreEqual(moviesList1[2].Genre, moviesList2[2].Genre);
-    //    Assert.AreEqual(moviesList1[2].Director, moviesList2[2].Director);
-    //    Assert.AreEqual(moviesList1[2].PremierDate, moviesList2[2].PremierDate);
-    //}
+
+
+    [TestMethod]
+    public void SaveAndLoadFromCsvFile()
+    {
+    
+        IRepo<Showing> showingRepo1 = new ShowingRepo();
+        IRepo<Showing> showingRepo2 = new ShowingRepo();
+
+
+        showingRepo1.RemoveAll();
+        showingRepo1.LoadFromFile();
+        showingRepo1.Add(showing);
+        showingRepo1.SaveToFile();
+
+        showingRepo2.LoadFromFile();
+
+        List<Showing> showingList1 = showingRepo1.GetAll().ToList();
+        List<Showing> showingList2 = showingRepo2.GetAll().ToList();
+
+
+
+
+        Assert.AreEqual(showingList1.Count, showingList2.Count);
+                  
+        for (int i = 0; i < showingList1.Count; i++) 
+        {
+        
+            Assert.AreEqual(showingList1[i].Movie.Title, showingList2[i].Movie.Title);
+            Assert.AreEqual(showingList1[i].Movie.Duration, showingList2[i].Movie.Duration);
+            Assert.AreEqual(showingList1[i].Movie.Genre, showingList2[i].Movie.Genre);
+            Assert.AreEqual(showingList1[i].Movie.Director, showingList2[i].Movie.Director);
+            Assert.AreEqual(showingList1[i].Movie.PremierDate, showingList2[i].Movie.PremierDate);
+
+            Assert.AreEqual(showingList1[i].Cinema.Name, showingList2[i].Cinema.Name);
+            Assert.AreEqual(showingList1[i].Cinema.City, showingList2[i].Cinema.City);
+            Assert.AreEqual(showingList1[i].Cinema.MaxCinemaHall, showingList2[i].Cinema.MaxCinemaHall);
+            int lastCinemaIndex = showingList1[i].Cinema.CinemaHalls.Count - 1;
+            Assert.AreEqual(showingList1[i].Cinema.CinemaHalls[lastCinemaIndex], showingList2[i].Cinema.CinemaHalls[lastCinemaIndex]);
+
+            Assert.AreEqual(showingList1[i].CinemaHall, showingList2[i].CinemaHall);
+            Assert.AreEqual(showingList1[i].ShowingDuration, showingList2[i].ShowingDuration);
+            Assert.AreEqual(showingList1[i].ShowingDate, showingList2[i].ShowingDate);
+        
+        }
+
+
+
+    }
 
 
 
